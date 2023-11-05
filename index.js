a5cee3403c7ce968f098f0e7f061df1f8d69b40a
Jonilson Sousa <jonilsonds9@gmail.com>
2023-11-05 11:07:09 -0300

    Adicionando geracao de token e env vars

--- index.js
+++ index.js
@@ -1,32 +1,74 @@
-import {Connection} from 'postgresql-client';
+const Connection = require("postgresql-client").Connection;
+const jwt = require('jsonwebtoken');
 
-async function database() {
-  const connection = new Connection('teste');
+async function cpfIsRegistered(cpf) {
+  const connection = new Connection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
   await connection.connect();
+  
+  const result = await connection.query(
+      'select * from clientes where cpf = $1',
+      {params: [cpf]});
 
-  const result = await connection.query('select * from clientes');
   const rows = result.rows;
 
-  console.log(rows);
-
-  await connection.close(); 
+  await connection.close();
+  
+  const response = rows.length > 0;
+  console.log(response);
+  
+  return response;
 }
 
-export const handler = async (event, context) => {
-  if (event.body) {
-    let body = JSON.parse(event.body);
-    console.log(body);
-    console.log(body.cpf);
+async function generateToken(cpf) {
+  const secret = Buffer.from(process.env.JWT_SECRET, 'base64');
+
+  const token = jwt.sign({ cpf }, secret, {
+    expiresIn: 600 // expires in 10min
+  });
+  
+  return token;
+}
+
+exports.handler = async (event, context) => {
+  if (!event.body) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify('Parâmetros inválidos ou incorretos!'),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+  }
+  
+  let body = JSON.parse(event.body);
+  console.log(body);
+  
+  if (await cpfIsRegistered(body.cpf)) {
+    const token = await generateToken(body.cpf);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ token: token }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
   }
 
-  await database();
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+  return {
+    statusCode: 401,
+    body: JSON.stringify('Parâmetros inválidos ou incorretos!'),
     headers: {
       'Content-Type': 'application/json',
     }
   };
-  return response;
 };
